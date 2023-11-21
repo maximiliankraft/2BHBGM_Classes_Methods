9b0ccba58c496e012c73a2798e59a56bbf08b847
Maximilian Kraft <kraft@spengergasse.at>
2023-11-21 08:53:36 +0100

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+
+            string msg = "Der Wert ist " + x;
+
+            Console.WriteLine(msg);
+            Console.WriteLine("Der wert ist {0}", x);
+            Console.WriteLine($"Der wert ist {x}");
+
+            Test t1 = new Test();
+
+            // t1.Property = "abc";
+            
+            t1.ProcessProperty();
+        }
+    }
+}
---
+++ ConsoleApplication1/Test.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApplication1
+{
+    public class Test
+    {
+        private string Property;
+
+        private string _name;
+
+        public bool IsSecret = false;
+        private string _lastName;
+
+        public string GetSecret(string secretValue)
+        {
+            if (IsSecret)
+            {
+                return "";
+            } else {
+                return secretValue;
+            }
+        }
+
+        public string Name
+        {
+            get => GetSecret(_name) ;
+            set => _name = value;
+        }
+
+        public string LastName
+        {
+            get => GetSecret(_lastName);
+            set => _lastName = value;
+        }
+
+        public void ProcessProperty()
+        {
+            this.Property = Property + Property;
+        }
+    }
+}
